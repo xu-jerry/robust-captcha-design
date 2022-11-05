d6534440128ae83fa52453a4b557718874423810
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2022-11-05 16:28:27 -0700

    Added distorted photos, code that displays 9 images

--- model.py
+++ model.py
@@ -8,19 +8,58 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 import pathlib
-archive_test_dir = pathlib.Path("archive/_test/")
-distorted_test_dir = pathlib.Path("DisCaptcha_v0/_test")
+training_dir = pathlib.Path("training/")
+# distorted_test_dir = pathlib.Path("DisCaptcha_v0/_test")
 
-categories = ["airplane", "car", "cat", "dog", "flower", "fruit", "motorbike", "person"]
-airplanes = list(archive_test_dir.glob('airplane/*'))
+# categories = ["airplane", "car", "cat", "dog", "flower", "fruit", "motorbike", "person"]
+# airplanes = list(archive_test_dir.glob('airplane/*'))
+classes = ["distorted", "original"]
 original = []
 distorted = []
-for category in categories:
-    original = original + list(archive_test_dir.glob(category + '/*'))
 
-distorted_categories = ["airplane", "cat"]
-for category in distorted_categories:
-    distorted = distorted + list(distorted_test_dir.glob(category + '/*'))
+image_count = len(list(training_dir.glob('*/*.jpg')))
+print(image_count)
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  training_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+  training_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+print(class_names)
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+plt.show()
+
+# for category in categories:
+#     original = original + list(archive_test_dir.glob(category + '/*'))
+
+# distorted_categories = ["airplane", "cat"]
+# for category in distorted_categories:
+#     distorted = distorted + list(distorted_test_dir.glob(category + '/*'))
 # PIL.Image.open(str(original[0])).show()
 # PIL.Image.open(str(distorted[0])).show()
 
@@ -28,7 +67,6 @@ batch_size = 32
 img_height = 180
 img_width = 180
 
-# classes = ["distorted", "nondistorted"]
 # print(str(original[0]))
 
 # train_ds = tf.keras.utils.image_dataset_from_directory(
