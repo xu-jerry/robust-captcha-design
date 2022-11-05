812699c3071dc021c063da5d9ec6b4f30f274874
Jerry Xu <54759887+xu-jerry@users.noreply.github.com>
2022-11-05 16:43:30 -0700

    Add creation of model

--- model.py
+++ model.py
@@ -54,6 +54,50 @@ for images, labels in train_ds.take(1):
     plt.axis("off")
 plt.show()
 
+for image_batch, labels_batch in train_ds:
+  print(image_batch.shape)
+  print(labels_batch.shape)
+  break
+
+normalization_layer = tf.keras.layers.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixel values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+num_classes = 2
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=3
+)
+
 # for category in categories:
 #     original = original + list(archive_test_dir.glob(category + '/*'))
 
@@ -63,9 +107,9 @@ plt.show()
 # PIL.Image.open(str(original[0])).show()
 # PIL.Image.open(str(distorted[0])).show()
 
-batch_size = 32
-img_height = 180
-img_width = 180
+# batch_size = 32
+# img_height = 180
+# img_width = 180
 
 # print(str(original[0]))
 
